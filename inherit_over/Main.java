84b7604989eed6c4428c7aa080013f21f3522688
elec139621 <85816256+elec139621@users.noreply.github.com>
2021-06-21 08:58:34 +0300

    Update Main.java

--- inherit_over/Main.java
+++ inherit_over/Main.java
@@ -20,6 +20,34 @@ public class Main {
 		printWithStarts("itay");
 		
 		Employee e = new Employee("dana", 1, 80000d);// new Person ( )
+		
+		
+		// create class MobilePhone
+		// protected String model
+		// protected String color
+		// getter/setter Alt +Shift + S -- not for ctor!!
+		// toString
+		// ctor (String model, String color)
+		// ctor (String model) -- calls other ctor with color "black"
+		// create dervied class IPhone
+		// protected float ios_version
+		// getter / setter
+		// to string 
+		// ctor (all base fields... , ios_version)
+		// create dervied class Samsung
+		// protected String android_candy_name
+		// getter / setter
+		// to string 
+		// ctor (all base fields... , android_candy_name)		
+		// Main:
+		// create iphone, android
+		// public static void showPhone(Android a) {
+		//   ... print android candy name }
+		// public static void showPhone(IPhone a) {
+		//   ... print ios_version }		
+		
+		
+		
 	}
 
 }
